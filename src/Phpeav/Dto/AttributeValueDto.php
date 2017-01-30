2863c91eca72bfc283a95c8f19490ac8bf039b59
Omar Alvarado <omaralvaradoluna@gmail.com>
2017-01-29 21:23:08 -0600

    #5 attribute value dto methods added

--- src/Phpeav/Dto/AttributeValueDto.php
+++ src/Phpeav/Dto/AttributeValueDto.php
@@ -38,4 +38,111 @@ class AttributeValueDto
      */
     protected $order;
 
+    /**
+     * @return mixed
+     */
+    public function getEntityId()
+    {
+        return $this->entityId;
+    }
+
+    /**
+     * @param mixed $entityId
+     * @return AttributeValueDto
+     */
+    public function setEntityId($entityId)
+    {
+        $this->entityId = $entityId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEntityTypeId()
+    {
+        return $this->entityTypeId;
+    }
+
+    /**
+     * @param mixed $entityTypeId
+     * @return AttributeValueDto
+     */
+    public function setEntityTypeId($entityTypeId)
+    {
+        $this->entityTypeId = $entityTypeId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAttributeId()
+    {
+        return $this->attributeId;
+    }
+
+    /**
+     * @param mixed $attributeId
+     * @return AttributeValueDto
+     */
+    public function setAttributeId($attributeId)
+    {
+        $this->attributeId = $attributeId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAttributesSetId()
+    {
+        return $this->attributesSetId;
+    }
+
+    /**
+     * @param mixed $attributesSetId
+     * @return AttributeValueDto
+     */
+    public function setAttributesSetId($attributesSetId)
+    {
+        $this->attributesSetId = $attributesSetId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param mixed $value
+     * @return AttributeValueDto
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @param mixed $order
+     * @return AttributeValueDto
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+        return $this;
+    }
 }
